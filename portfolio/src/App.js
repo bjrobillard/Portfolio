b22de80ec488ac76b2799c01be0fc907ac2d98ee
Ben Robillard <bjrobillard@mavs.coloradomesa.edu>
2024-08-12 16:40:11 -0600

    main components of page created... outline

---
+++ portfolio/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
